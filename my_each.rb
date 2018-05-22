96e69c2dc24519988bb50c491de4d83119dab768
Elaine Anderson <eparie@bu.edu>
2018-05-22 19:29:03 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,18 +1,3 @@
 
-
-def my_each(collection)
-  if block_given?
-    i = 0
-    while i < collection.length
-      yield(collection[i])
-      i = i + 1
-    end
-  collection
-  else
-    puts "Hey! No block was given!"
-  end
-end
-
-my_each(collection) do |i|
-puts i
+def my_each
 end
